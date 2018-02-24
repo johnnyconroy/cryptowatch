64d16ee2e1fbd7ac245c29109bbb43a8934b8ca8
johnnyconroy <jsknap@gmail.com>
2018-02-24 12:43:08 +0100

    add CLI coloured outputs

--- scripts/AWS/install-docker.sh
+++ scripts/AWS/install-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Coloured outputs
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
 # Install docker (see [here](https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository))
-echo ---------------------------------- install docker START
+printf "${YELLOW}---------------------------------- install docker START"
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -16,18 +20,18 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce
 sudo docker run hello-world
-echo ---------------------------------- install docker END
+printf "${YELLOW}---------------------------------- install docker END"
 
 # Install Docker-compose (see [here](https://docs.docker.com/compose/install/))
-echo ---------------------------------- install docker-compose START
+printf "${YELLOW}---------------------------------- install docker-compose START"
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-echo ---------------------------------- install docker-compose END
+printf "${YELLOW}---------------------------------- install docker-compose END"
 
 # Manage Docker as a non-root user (see [here](https://docs.docker.com/install/linux/linux-postinstall/))
-echo ---------------------------------- Manage Docker as a non-root user START
+printf "${YELLOW}---------------------------------- Manage Docker as a non-root user START"
 sudo groupadd docker
 sudo usermod -aG docker $USER
-echo ---------------------------------- Manage Docker as a non-root user END
-echo ---------------------------------- Log out and log back in so that your group membership is re-evaluated.
+printf "${YELLOW}---------------------------------- Manage Docker as a non-root user END"
+printf "${GREEN}---------------------------------- Log out and log back in so that your group membership is re-evaluated."
