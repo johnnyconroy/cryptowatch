0925c4b3391d4f93c2959d9c622b6ef863836c1e
johnnyconroy <jsknap@gmail.com>
2018-02-24 12:57:26 +0100

    add coloured outputs in docker install script

--- scripts/AWS/install-docker.sh
+++ scripts/AWS/install-docker.sh
@@ -3,9 +3,10 @@
 # Coloured outputs
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+DEFAULT='\033[0;37m'
 
 # Install docker (see [here](https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository))
-printf "${YELLOW}---------------------------------- install docker START"
+printf "${YELLOW}---------------------------------- install docker START \n${DEFAULT}"
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -20,18 +21,18 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce
 sudo docker run hello-world
-printf "${YELLOW}---------------------------------- install docker END"
+printf "${YELLOW}---------------------------------- install docker END \n${DEFAULT}"
 
 # Install Docker-compose (see [here](https://docs.docker.com/compose/install/))
-printf "${YELLOW}---------------------------------- install docker-compose START"
+printf "${YELLOW}---------------------------------- install docker-compose START \n${DEFAULT}"
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-printf "${YELLOW}---------------------------------- install docker-compose END"
+printf "${YELLOW}---------------------------------- install docker-compose END \n${DEFAULT}"
 
 # Manage Docker as a non-root user (see [here](https://docs.docker.com/install/linux/linux-postinstall/))
-printf "${YELLOW}---------------------------------- Manage Docker as a non-root user START"
+printf "${YELLOW}---------------------------------- Manage Docker as a non-root user START \n${DEFAULT}"
 sudo groupadd docker
 sudo usermod -aG docker $USER
-printf "${YELLOW}---------------------------------- Manage Docker as a non-root user END"
-printf "${GREEN}---------------------------------- Log out and log back in so that your group membership is re-evaluated."
+printf "${YELLOW}---------------------------------- Manage Docker as a non-root user END \n${DEFAULT}"
+printf "${GREEN}---------------------------------- Log out and log back in so that your group membership is re-evaluated. \n${DEFAULT}"
